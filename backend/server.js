40e9274194cbe3e4a1a02258199c7091108e3a1a
ndrhere <ndx199@gmail.com>
2025-12-07 13:28:52 +0530

    Adding project

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,11 @@ app.use("/api/contact", contactRoute)
 
 if(process.env.NODE_ENV === "production") {
   //server our react app
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   // __dirname is the root directory of our project.
 
   app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   })
   
 }
